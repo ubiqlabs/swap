3c9a034332ec95204d65128797378faa02086a23
Jaimish Patel <jaimish.patel95@gmail.com>
2021-11-22 11:18:25 -0500

    Update token lists URL

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/octanolabs/default-token-list/master/uniswap-default.tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/ubiqlabs/default-token-list/master/uniswap-default.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
