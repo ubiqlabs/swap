200c3057cc5a5d353dc30e5b7b4248158c0b244e
Jaimish Patel <jaimish.patel95@gmail.com>
2021-11-30 14:54:31 -0500

    Use assets url for base

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -2,20 +2,13 @@ import { Currency, ETHER, Token } from 'shinobi-sdk'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
-import EthereumLogo from '../../assets/images/ethereum-logo.png'
 import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
 
-const getTokenLogoURL = (address: string) =>
-  `https://raw.githubusercontent.com/ubiqlabs/assets/master/blockchains/ubiq/assets/${address}/logo.png`
+const getTokenLogoURL = (address?: string) =>
+  `https://raw.githubusercontent.com/ubiqlabs/assets/master/blockchains/ubiq/assets${address ? (`/` + address) : ``}/logo.png`
 
-const StyledEthereumLogo = styled.img<{ size: string }>`
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
-  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
-  border-radius: 24px;
-`
 
 const StyledLogo = styled(Logo)<{ size: string }>`
   width: ${({ size }) => size};
@@ -36,7 +29,7 @@ export default function CurrencyLogo({
   const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined)
 
   const srcs: string[] = useMemo(() => {
-    if (currency === ETHER) return []
+    if (currency === ETHER) return [getTokenLogoURL()]
 
     if (currency instanceof Token) {
       if (currency instanceof WrappedTokenInfo) {
@@ -47,10 +40,6 @@ export default function CurrencyLogo({
     }
     return []
   }, [currency, uriLocations])
-
-  if (currency === ETHER) {
-    return <StyledEthereumLogo src={EthereumLogo} size={size} style={style} />
-  }
 
   return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} />
 }
