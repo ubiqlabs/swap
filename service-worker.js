2af0b275ebcd451ff6bfc6bb7c69458b74043478
Jaimish Patel <jaimish.patel95@gmail.com>
2022-01-27 22:45:50 -0500

    1.0.2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.52dea06ab1d0a5a333e7c652bf821f13.js"
+  "./precache-manifest.e282de317aec3d1fe2984e6b270fad23.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/4.0059d5df.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpshinobi-interface"]=this["webpackJsonpshinobi-interface"]||[]).push([[4],{851:function(i,n){}}]);
+//# sourceMappingURL=4.0059d5df.chunk.js.map
